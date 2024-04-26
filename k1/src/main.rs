24ae288c9bfccb109bc2e5257c0c2b02e717737b
Eline Leske <eline@boehmert-leske.de>
2024-04-26 11:18:20 -0400

    read data

--- k1/src/main.rs
+++ k1/src/main.rs
@@ -1,3 +1,50 @@
+use std::env::args;
+use std::error::Error;
+use std::process;
+
+use serde::Deserialize;
+
+#[allow(dead_code, non_snake_case)]
+#[derive(Debug, Deserialize)]
+struct UserData {
+    age: i32,
+    gender: String,
+    hours_on_social_media: i32,
+    social_media: String,
+    interest: String,
+    profession: String,
+    income: i32,
+    indebt: bool,
+    house_owner: bool,
+    car_owner: bool,
+}
+
+fn example(path: &str, hh: bool) -> Result<(), Box<dyn Error>> {
+    println!("Using file {}", path);
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b',')
+        .has_headers(hh)
+        .flexible(true)
+        .from_path(path)?;
+    for result in rdr.deserialize() {
+        // Notice that we need to provide a type hint for automatic
+        // deserialization.
+        let record: UserData = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world! s");
+    let file = "social_media.csv";
+    let path: &str = &args[1];
+    let has_headers: &str = &args[2];
+    let mut hh = false;
+    if has_headers == "true" {
+       hh = true;
+    }
+    if let Err(err) = example(path, hh) {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
 }
---
+++ k1/target/debug/deps/k1-95d43332d7891a13.d
@@ -0,0 +1,5 @@
+/Users/elineleske/Desktop/BU/DS210/Final/k1/target/debug/deps/k1-95d43332d7891a13: src/main.rs
+
+/Users/elineleske/Desktop/BU/DS210/Final/k1/target/debug/deps/k1-95d43332d7891a13.d: src/main.rs
+
+src/main.rs:
